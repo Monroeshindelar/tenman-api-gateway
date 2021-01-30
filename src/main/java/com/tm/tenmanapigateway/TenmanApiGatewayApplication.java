24bbcc492836f11e57b2347eb8c1a78532b765d9
Monroe Shindelar <monroeshindelar@gmail.com>
2021-01-30 10:18:11 -0800

    Initial commit

---
+++ src/main/java/com/tm/tenmanapigateway/TenmanApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.tm.tenmanapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenmanApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenmanApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tm/tenmanapigateway/TenmanApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tm.tenmanapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenmanApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
